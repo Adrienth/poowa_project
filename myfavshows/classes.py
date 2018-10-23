cc2af2d8b37c0a0c941a7a0a74363ab9b01b789c
Adrienth <adrien.thomas.si@gmail.com>
2018-10-23 17:44:21 +0200

    nice style

--- myfavshows/classes.py
+++ myfavshows/classes.py
@@ -153,6 +153,8 @@ class Season:
     def _get_air_date(self):
         return self._air_date
 
+    def _get_episodes(self):
+        return self._episodes
 
     season_number = property(_get_season_number)
     name = property(_get_name)
@@ -162,6 +164,7 @@ class Season:
     poster_url = property(_get_poster_url)
     episode_count = property(_get_episode_count)
     air_date = property(_get_air_date)
+    episodes = property(_get_episodes)
 
 
 class Episode:
@@ -195,7 +198,7 @@ class Episode:
         if self._poster_path is None:
             return None
         else:
-            return 'https://image.tmdb.org/t/p/w200' + self._poster_path
+            return 'https://image.tmdb.org/t/p/w300' + self._poster_path
 
     def _get_vote_average(self):
         return self._vote_average
@@ -210,3 +213,101 @@ class Episode:
     poster_url = property(_get_poster_url)
     air_date = property(_get_air_date)
     vote_average = property(_get_vote_average)
+
+"""
+class Cheese():
+    def __init__(self, *args, **kwargs):
+        #args -- tuple of anonymous arguments
+        #kwargs -- dictionary of named arguments
+        self.num_holes = kwargs.get('num_holes',random_holes())
+
+
+Here is the list that needs to be completed of parameters for a show_quick_view (-- : we ignore) :
+poster_path
+popularity
+id
+vote_average
+vote_count
+overview
+first_air_date
+name
+--backdrop_path
+--original_name
+--origin_country
+--genre_ids
+--original_language
+
+
+Here is the list that needs to be completed of parameters for a show_full_view (-- : we ignore) :
+poster_path
+popularity
+id
+vote_average
+vote_count
+overview
+first_air_date
+name
+seasons(
+    air_date,
+    episode_count,
+    id,name,
+    overview,
+    poster_path,
+    season_number)
+next_episode_to_air(
+    air_date,
+    episode_number,
+    id,name,
+    overview,
+    production_code,
+    season_number,
+    show_id,
+    still_path,
+    vote_average,
+    vote_count)
+created_by (
+    id,
+    credit_id,
+    name,
+    gender,
+    profile_path)
+status
+--original_name
+--origin_country
+--genres(
+    id,
+    name)
+--original_language
+--backdrop_path
+--episode_run_time
+--homepage
+--in_production
+--languages
+--last_air_date
+--last_episode_to_air(
+    air_date,
+    episode_number,
+    id,name,
+    overview,
+    production_code,
+    season_number,
+    show_id,
+    still_path,
+    vote_average,
+    vote_count)
+
+--networks(
+    name,
+    id,
+    logo_path,
+    origin_country)
+--number_of_episodes
+--number_of_seasons
+--production_companies(
+    id, 
+    logo_path,
+    name,
+    origin_country)
+--status
+--kind
+"""
--- myfavshows/myshow.py
+++ myfavshows/myshow.py
@@ -19,28 +19,13 @@ def get_my_show(show_id):
         shows_to_session()
 
     show = ShowDetailedView(show_id)
-    seasons = []
-    for season in show['seasons']:
-        if season['poster_path']:
-            season['poster_url'] = 'https://image.tmdb.org/t/p/w200' + season['poster_path']
-        seasons.append(season)
-    return render_template('myshow/myshow.html', show=show, seasons=seasons)
+
+    return render_template('myshow/myshow.html', show=show)
 
 
 @bp.route('/myshow/<int:show_id>/season/<int:season_number>')
 def get_my_season(show_id,season_number):
 
-    season = get_season(show_id,season_number)
+    season = Season(show_id, season_number)
 
-    if season['poster_path']:
-        season['poster_url'] = 'https://image.tmdb.org/t/p/w200' + season['poster_path']
-
-    episodes = []
-
-    for episode in season['episodes'] :
-        if episode['still_path']:
-            episode['poster_url'] = 'https://image.tmdb.org/t/p/w200' + episode['still_path']
-        episodes.append(episode)
-    season['episode_count'] = len(episodes)
-
-    return render_template('myshow/myseason.html', season=season, episodes=episodes)
+    return render_template('myshow/myseason.html', season=season)
