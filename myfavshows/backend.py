7e4670ae9f1021cae6d97234e1689ee12d2071e4
charlesdlrsa <charlesdlrsa@hotmail.fr>
2018-10-23 15:43:47 +0200

    Création des classes Show et ShowDetailedView Modification du module backend.py et de base.html en conséquence. Déplacement des fonctions add_to_fav et rm_from_fav dans myfav.py

--- myfavshows/backend.py
+++ myfavshows/backend.py
@@ -6,6 +6,7 @@ from werkzeug.exceptions import abort
 
 import requests
 from myfavshows.db import get_db
+from myfavshows.classes import *
 
 params = {'api_key': '7ecd6a3ceec1b96921b4647095047e8e'}
 
@@ -36,79 +37,8 @@ def get_shows_from_search(query):
         pass
     else:
         for res in req_json["results"]:
-            results += [select_useful_items(res)]
+            results += [Show(res)]
     return results
-
-
-def select_useful_items(res):
-    """
-    :param res: one of the dictionary included in the req_json object, which is a list of dictionaries
-    :return: a dictionary selecting the useful items of the input and cleaning them
-    """
-
-    result = {
-        'id': res['id'],
-        'title': res['name'],
-        'date': res['first_air_date'],
-        'popularity': res['popularity'],
-        'vote_average': res['vote_average'],
-        'poster_path': res['poster_path'],
-        'overview': res['overview']
-        }
-
-    result = clean_result(result)
-
-    return result
-
-
-def get_show_from_id(show_id):
-    """
-    :param show_id:
-    :return: a dictionary (about the show searched) with the following items : 'title', 'date', 'popularity',
-    'vote_average', 'overview', 'id', 'poster_url', 'number of seasons' ...
-    """
-
-    req = requests.get('https://api.themoviedb.org/3/tv/' + str(show_id), params)
-    req_json = req.json()
-    result = {
-        'id': show_id,
-        'title': req_json['name'],
-        'date': req_json['first_air_date'],
-        'popularity': req_json['popularity'],
-        'vote_average': req_json['vote_average'],
-        'poster_path': req_json['poster_path'],
-        'overview': req_json['overview'],
-        'number_of_seasons': req_json['number_of_seasons'],
-        'number_of_episodes': req_json['number_of_episodes'],
-        'seasons': req_json['seasons'],
-        'next_episode_to_air': req_json['next_episode_to_air']
-        }
-
-    result = clean_result(result)
-
-    return result
-
-
-def clean_result(result):
-    """
-    Cleans the dictionnary of a tv show by adapting it to our needs like clean poster URL and truncated overview field
-    :param result: the dictionnary representing a tv show
-    :return: result the same cleaned dictionnary
-    """
-    # Check if there is poster path before creating the URL
-    if result['poster_path'] is None:
-        result['poster_url'] = None
-    else:
-        result['poster_url'] = 'https://image.tmdb.org/t/p/w200' + result['poster_path']
-
-    # Truncates the overview text to fit our style need, 260 characters max
-    nb_char = 270
-    view = result['overview']
-    if len(view) > nb_char:
-        view = view[:nb_char] + '...'
-    result['trunc_overview'] = view
-
-    return result
 
 
 def shows_to_session():
--- myfavshows/myshow.py
+++ myfavshows/myshow.py
@@ -1,11 +1,12 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for,session
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
 from werkzeug.exceptions import abort
 
-from myfavshows.auth import login_required
 from myfavshows.db import get_db
 from myfavshows.backend import *
+from myfavshows.classes import *
+
 
 import requests
 
@@ -17,5 +18,5 @@ params = {'api_key': '7ecd6a3ceec1b96921b4647095047e8e'}
 @bp.route('/myshow/<int:show_id>')
 def get_my_show(show_id):
 
-    result = [get_show_from_id(show_id)]
+    result = [ShowDetailedView(show_id)]
     return render_template('myshow/myshow.html', results=result)
